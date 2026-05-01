6e1c8ab1ffe857a0f694c580e6952ea748cce036
StarAel <153726548+StarAel@users.noreply.github.com>
2026-05-02 03:12:01 +0800

    Created Base Record dataType

---
+++ record.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+struct Record {
+    std::string ID;
+    int Age;
+    std::string modeOfTransport;
+    int dailyDistance;
+    float carbonEmission;
+    int monthlyFrequency;
+};
