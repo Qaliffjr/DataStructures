48988ea3a034a9260d8252ae617c8403be3f2cb6
StarAel <153726548+StarAel@users.noreply.github.com>
2026-05-02 03:21:10 +0800

    Delete CSVParser.hpp

--- CSVParser.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ResidentArray.hpp"
-
-double computeMonthlyEmissions(double dailyDistance, double emissionFactor, int averageDaysPerMonth);
-
-bool parseResidentCsvLine(const char* line, Resident& outResident);
-
-bool loadCsvToArray(const char* filePath, ResidentArray& outArray);
